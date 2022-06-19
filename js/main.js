c2233d55dff8cf2d0aa367011a13f58ca62aff96
v1mer123331 <107111283+v1mer123331@users.noreply.github.com>
2022-06-19 16:39:58 +0300

    BlogArt

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо за Обращение!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 750);
+		});
+		return false;
+	});
+
+});
+
+
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
